bf5ab30cada22f6bd38d267327a86fc5d9e158ba
C0dyS <sashaculle@gmail.com>
2024-03-26 19:40:21 +0100

    3 task

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
-class Passport:
-    def __init__(self,name,age,id_number):
+class Animal:
+    def __init__(self,name,age,weight_kg):
         self._name = name
+        self._weight_kg = weight_kg
         self._age = age
-        self._id_number = id_number
     def get_data(self):
-        return self._name,self._age,self._id_number
-    def change_id_number(self,new_id_number):
-        self._id_number = new_id_number
-
-class ForeignPassport(Passport):
-    def __init__(self,name,age,foreign_passport_id_number):
-        super().__init__(name,age,'')
-        print('parent init')
-        self._foreign_passport_id_number = foreign_passport_id_number
-    def get_data(self):
-        name,age, _ = super().get_data()
-        return name,age,self._foreign_passport_id_number
-    def change_foreign_id_number(self,new_id_number):
-        self._foreign_passport_id_number = new_id_number
+        return self._name,self._age,self._weight_kg
+    def change_age(self,new_age):
+        self._age = new_age
+    def change_weight(self,new_weight_kg):
+        self._weight_kg = new_weight_kg
+    def eat(self,food_in_kg):
+        self._weight_kg += food_in_kg
+    def change_name(self,new_name):
+        self._name = new_name
+class Tiger(Animal):
+    def __init__(self,name,age,weight_kg):
+        super().__init__(name,age,weight_kg)
+    def Pounce(self):
+        print('Pounce')
+    def Look_majestic(self):
+        print('tiger looks majestic')
+class Crocodile(Animal):
+    def __init__(self,name,age,weight_kg):
+        super().__init__(name,age,weight_kg)
+    def sleep(self):
+        print('crocodile sleeps')
+class Kangaroo(Animal):
+    def __init__(self,name,age,weight_kg):
+        super().__init__(name,age,weight_kg)
+    def jump(self):
+        print('Kangaroo jumps')
