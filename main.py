337cecc8bdc681948503c7a251a40cb80aaa2a76
C0dyS <sashaculle@gmail.com>
2024-03-26 19:55:35 +0100

    4 task

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
-class Animal:
-    def __init__(self,name,age,weight_kg):
-        self._name = name
-        self._weight_kg = weight_kg
-        self._age = age
+class Clock:
+    def __init__(self,hours,minutes,):
+        self._minutes = minutes
+        self._hours = hours
     def get_data(self):
-        return self._name,self._age,self._weight_kg
-    def change_age(self,new_age):
-        self._age = new_age
-    def change_weight(self,new_weight_kg):
-        self._weight_kg = new_weight_kg
-    def eat(self,food_in_kg):
-        self._weight_kg += food_in_kg
-    def change_name(self,new_name):
-        self._name = new_name
-class Tiger(Animal):
-    def __init__(self,name,age,weight_kg):
-        super().__init__(name,age,weight_kg)
-    def Pounce(self):
-        print('Pounce')
-    def Look_majestic(self):
-        print('tiger looks majestic')
-class Crocodile(Animal):
-    def __init__(self,name,age,weight_kg):
-        super().__init__(name,age,weight_kg)
-    def sleep(self):
-        print('crocodile sleeps')
-class Kangaroo(Animal):
-    def __init__(self,name,age,weight_kg):
-        super().__init__(name,age,weight_kg)
-    def jump(self):
-        print('Kangaroo jumps')
+        return self._hours,self._minutes
+    def change_minutes(self,new_minutes):
+        self._minutes = new_minutes
+    def change_hours(self,new_hours):
+        self._hours = new_hours
+class AnalogClock(Clock):
+    def __init__(self, hours, minutes):
+        super().__init__(hours, minutes)
+
+    def display_time(self):
+        if self._hours < 12:
+            print(f'{self._hours}:{self._minutes} AM')
+        else:
+            print(f'{self._hours - 12}:{self._minutes} PM')
+class DigitalClock(Clock):
+    def __init__(self,hours,minutes):
+        super().__init__(hours,minutes)
+    def display_time(self):
+        print(f'{self._hours}:{self._minutes}')
+
+clock1 = AnalogClock(24,30)
+clock2 = AnalogClock(12,00)
+clock3 = DigitalClock(19,30)
+clock1.display_time()
+clock2.display_time()
+clock3.display_time()
